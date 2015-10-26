fa4fe5b125573ec5e8e6c06be66e4b22c63e0f93
Johann Cohen-Tanugi <cohen@slac.stanford.edu>
2015-10-26 13:35:14 +0100

    add the possibility to point to iRODS

--- aspic_testing/v0/producer_aspic_testing.py
+++ aspic_testing/v0/producer_aspic_testing.py
@@ -3,16 +3,25 @@ import os, glob
 from lcatr.harness import config
 print "executing producer_test_job.py"
 
+IRODS = True
 #this is temporary stuff to get going
 cfg=config.Config()
-
 basedir = os.environ['ASPIC_BASE_DIR']
+uid = cfg.unit_id
 
-input_file = glob.glob(os.path.join(basedir,"Logs","log-%s-*.txt"%cfg.unit_id))[0]
+logdir = os.path.join(basedir,"Logs")
+chipdir = os.path.join(basedir,"CHIP%s"%uid)
+if not IRODS:
+    input_file = glob.glob(os.path.join(logdir,"log-%s-*.txt"%uid))[0]
+    os.system('cp %s .'%input_file)
+    os.system('ln -s %s .'%rel_path)
+else :
+    ss = "filename=`ils %s | grep log-%s`; iget %s/`echo $filename`"%(logdir,uid\
+,logdir)
+    os.system(ss)
+    input_file = glob.glob('log-%s*'%uid)[0]
+    os.system('iget -r %s'%chipdir)
+
 print "copying %s to current directory"%input_file
-os.system('cp %s .'%input_file)
 
-input_info=os.path.basename(input_file).split('-')
-rel_path = os.path.join(basedir,"CHIP%s"%input_info[1])
-print "creating symlink to %s"%rel_path
-os.system('ln -s %s .'%rel_path)
+
