7cddfd2e873bae08d17388750ab18cb1c24134dc
Johann Cohen-Tanugi <cohen@slac.stanford.edu>
2015-10-30 15:43:07 +0100

    use siteUtils instead of lcatr.config to retrieve unit-id and fix the switch to SPS or IRODS

--- aspic_testing/v0/producer_aspic_testing.py
+++ aspic_testing/v0/producer_aspic_testing.py
@@ -1,21 +1,23 @@
 #!/usr/bin/env python
 import os, glob
-from lcatr.harness import config
+import siteUtils
 print "executing producer_test_job.py"
 
 IRODS = True
-#this is temporary stuff to get going
-cfg=config.Config()
-basedir = os.environ['ASPIC_BASE_DIR']
-uid = cfg.unit_id
 
-logdir = os.path.join(basedir,"Logs")
-chipdir = os.path.join(basedir,"CHIP%s"%uid)
+uid = siteUtils.getUnitId()
+
 if not IRODS:
+    basedir = "/sps/lsst/DataBE/ASPIC_production"
+    logdir = os.path.join(basedir,"Logs")
+    chipdir = os.path.join(basedir,"CHIP%s"%uid)
     input_file = glob.glob(os.path.join(logdir,"log-%s-*.txt"%uid))[0]
     os.system('cp %s .'%input_file)
-    os.system('ln -s %s .'%rel_path)
+    os.system('ln -s %s .'%chipdir)
 else :
+    basedir = "/lsst-fr/home/lsstcamera/ASPIC_production"
+    logdir = os.path.join(basedir,"Logs")
+    chipdir = os.path.join(basedir,"CHIP%s"%uid)
     ss = "filename=`ils %s | grep log-%s`; iget %s/`echo $filename`"%(logdir,uid\
 ,logdir)
     os.system(ss)
