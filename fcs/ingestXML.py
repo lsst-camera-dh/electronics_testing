48636bb80421a16efaf27fe28c84efc02e2a1ad7
Johann Cohen-Tanugi <cohen@slac.stanford.edu>
2016-04-02 23:33:16 +0200

    new hardwareType defiition API

--- fcs/ingestXML.py
+++ fcs/ingestXML.py
@@ -1,9 +1,13 @@
 from openpyxl import load_workbook
 from  eTraveler.clientAPI.connection import Connection
-import yaml, os
+import yaml, os, sys
 
 savedir='test'
 excel_file='AutoChanger_BOM_simplifiee_20151009.xlsx'
+subsystem = 'CHGR'
+if not os.path.exists(savedir):
+    os.mkdir(savedir)
+print "yaml files will be saved in dir ", savedir
 
 myConn = Connection('jrb', 'Raw', prodServer=False)
 
@@ -13,17 +17,19 @@ class yamlAssembly():
         self._yaml = yaml.load(open("assembly_template.yml"))
         self._name = (record[3].value).encode('utf-8')
         self._yaml['HardwareGroup'] = self._name
-        self.registerHardware(self._name, record)
+        self.registerHardwareType(self._name, record)
 
-    def registerHardware(self, name, rec):
+    def registerHardwareType(self, name, rec):
         newId = ''
         try: 
             newId=myConn.defineHardwareType(name=name, 
                                             description=rec[7].value,
+                                            subsystem=subsystem,
+                                            batchedFlag=1,
                                             sequenceWidth='0')
             print 'New hardware type defined.  Returned id is ', newId
         except Exception,msg:
-            print 'Hardware type definition failed with exception ',msg
+             print 'Hardware type %s definition failed with exception : %s'%(name,msg.message)
 
     def registerRelationship(self, relname, name):
         newId = ''
@@ -37,15 +43,25 @@ class yamlAssembly():
                                           )
             print 'New relationship defined with id ', newId
         except Exception, msg:
-            print 'Relationship definition failed with exception ', msg 
-    def save(self):
-        with open(os.path.join(savedir,'%s.yaml'%self._name), 'w') as outfile:
+            print 'Relationship definition %s failed with exception : %s'%(relname,msg.message) 
+
+    def save(self, ingest=False):
+        filepath=os.path.join(savedir,'%s.yaml'%self._name)
+        with open(filepath, 'w') as outfile:
             outfile.write( os.path.join(savedir,yaml.dump(self._yaml, default_flow_style=False)) )
-    
+            if ingest is True:
+                msg = myConn.validateYaml(filepath)
+                if msg==0:
+                    errorCode = myConn.uploadYaml(filepath, reason="")
+                    if errorCode !=0:
+                        print "ingestion failed with code :", errorCode
+                else:
+                    "file %s validation failed with error code: %d"%(self._name, msg)
+            
     def addRelationship(self, gg):
         name = (gg[3].value).encode('utf-8')
         if gg[0].value == 3:
-            self.registerHardware(name, gg)#the deepest elements need registration
+            self.registerHardwareType(name, gg)#the deepest elements need registration
         sequence = self._yaml['Sequence'][0]
         relname = '%s-%s'%(self._name, name)
         new_relationship = {'RelationshipName':relname, 'RelationshipAction':'install'}
@@ -57,6 +73,7 @@ class yamlAssembly():
 
 
 try :
+    print "loading the workbook ", excel_file
     wb = load_workbook(filename = excel_file, read_only=True)
     ws = wb.active
     gen = ws.iter_rows()
@@ -91,7 +108,7 @@ for gg in gen:
         d2 = yamlAssembly(gg)
     if gg[0].value == 3:
         d2.addRelationship(gg)
-d0.save()
+d0.save(ingest=False)
     #     print gg[3].value 
     #     for gg in gen:
     #         if gg[0].value==2:
